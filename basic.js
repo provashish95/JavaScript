79c7430eb3bdfb29184e9f05e8c1ef91b8074b2b
Provashish Roy <roytonmoy95@gmail.com>
2024-06-09 00:46:40 +0600

    operator

--- basic.js
+++ basic.js
@@ -5,3 +5,20 @@ let y = 20;
 function colorStyle(){
     document.getElementById('demo').style.color='red';
 }
+
+//concatenation in js
+let text1 = "Provashish";
+let text2 = "Roy"
+let text = text1 + ' '+text2;
+text1 += text2;
+
+//adding number and string 
+let sum = 5 + 5; 
+let sum1 = "5" + 5; 
+let string = "hello" + 5;
+
+console.log(sum);
+console.log(sum1);
+console.log(string);
+
+
---
+++ bitwise-operator.js
@@ -0,0 +1,9 @@
+let num1 = 8; 
+let num2 = 6; 
+// let res1 = num1.toString(2); 
+// let res2 = num2.toString(2);
+let result = num1 & num2; 
+
+let number = 77 ; 
+let oct = number.toString(8);
+console.log(oct);
