b88b82238d9b4d678c4419d0efdb3d2df62f4e86
Provashish Roy <roytonmoy95@gmail.com>
2024-06-09 23:44:46 +0600

    data type

--- bitwise-operator.js
+++ bitwise-operator.js
@@ -6,4 +6,25 @@ let result = num1 & num2;
 
 let number = 77 ; 
 let oct = number.toString(8);
-console.log(oct);
+// console.log(oct);
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
