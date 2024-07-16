7ee97573115bbf8429563e20aea96b9dd2445d3f
provashish95 <provashishroy95@gmail.com>
2024-07-16 12:24:48 +0600

    learn object

---
+++ object.js
@@ -0,0 +1,41 @@
+//Normal object here...
+const persone = {
+    name: "Provashish", 
+    age : 28, 
+    job : "IT", 
+    Address: "Mirpur-1", 
+    phone : "01766552407"
+}; 
+// console.log(persone.name);
+
+//empty object here...
+const employee = {}; 
+employee.name = "Provashish"; 
+employee.age = 28; 
+// console.log(employee.name);
+
+//using new keyword for declare object..
+const jobHolder = new Object(); 
+jobHolder.name = "Provashish";
+// console.log(jobHolder.name); 
+// console.log(jobHolder['name']); 
+
+
+//Object method 
+const personeOne = {
+    firstName : "Provashish", 
+    lastName : "Roy",
+    id: 27,
+    fullName: function fullName(){
+        return this.firstName + " "+ this.lastName + " and my ID is: " + this.id;
+    } 
+}
+// console.log(personeOne.fullName());
+
+const demoOne = {
+    name: "Provashish", 
+    age: 28
+}
+const x = demoOne; 
+x.age = 30;
+console.log(demoOne.age);
