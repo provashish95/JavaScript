0eb8fc6e6b16ebeafdfb86a7e1bd9829364b7b49
provashish95 <provashishroy95@gmail.com>
2024-07-17 11:38:06 +0600

    basic string

---
+++ string.js
@@ -0,0 +1,6 @@
+let text = `Provashish Roy`; 
+let text2 = `Provashish Roy ${10 + 20}`; 
+let longText = "Hi I am provashish and my passion \"programming\" and my home is \\ghior\\ "; 
+let length = text.length;
+
+console.log(text === text2);
