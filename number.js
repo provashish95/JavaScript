2327d1ad2f1e83b1b24a0206293e13d5299762b1
provashish95 <provashishroy95@gmail.com>
2024-07-30 17:49:41 +0600

    updated

--- number.js
+++ number.js
@@ -1,6 +1,11 @@
 let x = "10"; 
 let num = 20;
 let notNumber = "I am provashish";
+// console.log(isNaN(num));
+// console.log(typeof(NaN));
+let num1 = 2; 
+let num2 = 0; 
+// console.log(num1 / num2);
 
-console.log(isNaN(num));
-console.log(typeof(NaN));
+console.log(typeof(typeof(Infinity)));
+
