1ae495606659eac0f857441d8013eded00555386
Provashish Roy <roytonmoy95@gmail.com>
2024-08-03 00:15:31 +0600

    problem solving

---
+++ problemSolve.js
@@ -0,0 +1,55 @@
+//Ludo game from 1 to 6...
+function getIntegerNumber(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min; 
+}
+console.log(getIntegerNumber(1,6));
+
+//Print names by alphabetically... 
+const names = ['tonmoy', 'provashish', 'riya', 'shipan', 'shuvo', 'gopal', 'sumit']; 
+const sortingNames = names.sort();
+for(let name of sortingNames){
+    console.log(name);
+}
+
+//print numbers are minimum to maximum...
+const numbers = [20, 10, 30, 4, 5, 6,19, 2, 1, 90];
+const sortingNumber = numbers.sort(myFunction);
+function myFunction (a, b){ 
+    return a - b; 
+}
+for(let number of sortingNumber){
+    console.log(number);
+}
+
+//Leap year find out ...
+function isLeapYear(year){
+    if((year % 400 === 0 ) || ((year % 4 === 0 ) && (year % 100 !== 0))){
+        console.log(year + " is Leap Year");
+    }else{
+        console.log(year + " is not Leap Year");
+    }
+}
+isLeapYear(2020);
+
+//please find out how many vowels are present in a sentence...
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E','I','O', 'U'];
+
+function getTotalVowels(sentence){
+    let count =0; 
+    const letters = Array.from(sentence);
+    letters.forEach(function(value, index, array){
+        if(vowels.includes(value)){
+            count++;
+        }
+    });
+    return count;
+}
+console.log(getTotalVowels('I love Bangladesh'));
+
+//get duplicate number from an array...
+const number = [1,2,3,4,5,5,6,7,6,8,9,9,9,1,2]; 
+
+const duplicate = number.filter(function(value, index, array){
+ return array.indexOf(value) !== index;
+})
+console.log(duplicate);
